209f4af1dc47588331863086cd787ed109bf61b6
Michael Stucki <michael.stucki@typo3.org>
2014-02-02 23:55:33 +0100

    Always connect to the same container

--- run.sh
+++ run.sh
@@ -3,9 +3,11 @@
 cd $(dirname $0)
 
 SOURCE=$(pwd)/android
+CONTAINER=cyanogenmod
+REPOSITORY=stucki/cyanogenmod
 
 test -d $SOURCE || mkdir $SOURCE
 
-docker run -v $SOURCE:/home/cmbuild/android -i -t stucki/cyanogenmod sh -c "exec >/dev/tty 2>/dev/tty </dev/tty && /bin/bash"
+docker start -i $CONTAINER 2>/dev/null || docker run -v $SOURCE:/home/cmbuild/android -i -t -name $CONTAINER $REPOSITORY sh -c "exec >/dev/tty 2>/dev/tty </dev/tty && /bin/bash"
 
 exit $?
