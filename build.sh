1d4a3f21187fe9c53e7b5c5e3ef48300c1c2b94a
Michael Stucki <michael.stucki@typo3.org>
2014-02-16 12:50:24 +0100

    Add comments

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ cd $(dirname $0)
 
 SOURCE=$(pwd)/android
 
+# Create a shared folder which will be used as working directory.
 test -d $SOURCE || mkdir $SOURCE
 
 docker build -t stucki/cyanogenmod .
