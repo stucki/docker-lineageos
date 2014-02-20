529e27786c208b1c6aa2c3bbd1508c09ac6e0253
Michael Stucki <michael.stucki@typo3.org>
2014-02-20 12:09:08 +0100

    Remove workaround for running screen (has been fixed in Docker 0.8.1)

--- run.sh
+++ run.sh
@@ -10,8 +10,6 @@ REPOSITORY=stucki/cyanogenmod
 test -d $SOURCE || mkdir $SOURCE
 
 # Try to start an existing/stopped container with the given name $CONTAINER. Otherwise, run a new one.
-# The exec stuff at the end is a workaround which is currently needed (docker 0.8.0) to run screen within docker...
-# see https://github.com/dotcloud/docker/issues/728
-docker start -i $CONTAINER 2>/dev/null || docker run -v $SOURCE:/home/cmbuild/android -i -t -name $CONTAINER $REPOSITORY sh -c "exec >/dev/tty 2>/dev/tty </dev/tty && screen -s /bin/bash"
+docker start -i $CONTAINER 2>/dev/null || docker run -v $SOURCE:/home/cmbuild/android -i -t -name $CONTAINER $REPOSITORY "screen -s /bin/bash"
 
 exit $?
