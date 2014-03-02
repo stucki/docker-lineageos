6416e726037caa8e08b3592da68a7cc49e952a67
Michael Stucki <michael.stucki@typo3.org>
2014-03-02 13:00:27 +0100

    Do not use cache when building the image

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ SOURCE=$(pwd)/android
 # Create a shared folder which will be used as working directory.
 test -d $SOURCE || mkdir $SOURCE
 
-docker build -t stucki/cyanogenmod .
+docker build --no-cache -t stucki/cyanogenmod .
 
 exit $?
