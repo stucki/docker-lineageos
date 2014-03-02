4ed5aed97f2af2a0c60370d26440846d81383a6d
Michael Stucki <michael.stucki@typo3.org>
2014-03-02 13:09:37 +0100

    Remove intermediate containers during "docker build"

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ SOURCE=$(pwd)/android
 # Create a shared folder which will be used as working directory.
 test -d $SOURCE || mkdir $SOURCE
 
-docker build --no-cache -t stucki/cyanogenmod .
+docker build --no-cache --rm -t stucki/cyanogenmod .
 
 exit $?
