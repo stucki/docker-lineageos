60b182cbe0092bcc84b186022c0b1c7fc88e76bf
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2014-03-31 15:33:33 +0200

    Fix "docker --name" command line option.
    Option "-name" to docker is obsolete.
    
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ REPOSITORY=stucki/cyanogenmod
 test -d $SOURCE || mkdir $SOURCE
 
 # Try to start an existing/stopped container with the given name $CONTAINER. Otherwise, run a new one.
-docker start -i $CONTAINER 2>/dev/null || docker run -v $SOURCE:/home/cmbuild/android -i -t -name $CONTAINER $REPOSITORY sh -c "screen -s /bin/bash"
+docker start -i $CONTAINER 2>/dev/null || docker run -v $SOURCE:/home/cmbuild/android -i -t --name $CONTAINER $REPOSITORY sh -c "screen -s /bin/bash"
 
 exit $?
