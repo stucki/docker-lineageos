831eb4f7c6bf61be7fbb051cfc10b854d173e103
Michael Stucki <michael.stucki@typo3.org>
2016-03-13 15:00:47 +0100

    Remove permission fix (no longer needed)

--- startup.sh
+++ startup.sh
@@ -6,15 +6,5 @@ if [ ! -f /srv/ccache/CACHEDIR.TAG ]; then
 	CCACHE_DIR=/srv/ccache ccache -M 50G
 fi
 
-# Fix permissions. This is needed temporarily to migrate old projects which were still checked out as root.
-# It can be removed at some point in the future.
-# For performance reasons, only search on the first level.
-for i in "/home/cmbuild/android" "/srv/ccache"; do
-	if [ $(find $i -maxdepth 1 -user root | wc -l) -gt 0 ]; then
-		echo "Fixing permissions on $i..."
-		chown -R cmbuild:cmbuild $i
-	fi
-done
-
 # Launch screen session
 screen -s /bin/bash
