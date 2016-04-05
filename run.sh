967b20f02e3d7957efffd505b05d5c2b5eb93c7a
Michael Stucki <michael.stucki@typo3.org>
2016-04-05 21:49:12 +0200

    Cleanup

--- run.sh
+++ run.sh
@@ -27,23 +27,6 @@ while [[ $# > 0 ]]; do
 	shift
 done
 
-
-while [[ $# > 0 ]]
-do
-key="$1"
-
-case $key in
-    -r|--rebuild)
-    FORCE_BUILD=1
-    ;;
-    *)
-    shift # past argument or value
-    ;;
-esac
-shift
-
-done
-
 # Create shared folders
 mkdir -p $SOURCE
 mkdir -p $CCACHE
@@ -70,7 +53,7 @@ elif [[ $FORCE_BUILD = 1 ]] || ! echo "$IMAGE_EXISTS" | grep -q "$TAG"; then
 fi
 
 if [[ $OK -ne 0 ]]; then
-	exit 1;
+	exit 1
 fi
 
 # With the given name $CONTAINER, reconnect to running container, start
